31a90f03c4b069b60f42f1b851b9a79f538af738
Rinkal <rinkaladesara.tops@gmail.com>
2026-02-14 13:03:53 +0530

    .

--- Project/controllers/subcatController.js
+++ Project/controllers/subcatController.js
@@ -1,13 +1,19 @@
 const subcatModel = require('../models/subcatModel')
 // import subcatModel from "../models/subcatModel.js"
 const insSubcat=async(req,res)=>{
-    let ans = await subcatModel.insertOne(req.body)
+    let id = req.body.subid
+    if(id!=''){
+        let ans = await subcatModel.findByIdAndUpdate(id,req.body)
+    } else {
+        let ans = await subcatModel.insertOne(req.body)
+    }
     res.redirect('/subcategory')
 }
 const dispSubcat=async(req,res)=>{
     let data = await subcatModel.find()
     res.render('subcategory',{
-        "subcatdata":data
+        "subcatdata":data,
+         "editdata":""
     })
 }
 const delSubcat = async(req,res)=>{
@@ -15,4 +21,14 @@ const delSubcat = async(req,res)=>{
     let data = await subcatModel.findByIdAndDelete(id)
     res.redirect('/subcategory')
 }
-module.exports = {insSubcat,dispSubcat,delSubcat}
+
+const editSubcat = async(req,res)=>{
+   const id = req.params.id
+     let editdata = await subcatModel.findById(id)
+    let data = await subcatModel.find()
+    res.render('subcategory',{
+        subcatdata:data,
+        editdata:editdata
+    })
+}
+module.exports = {insSubcat,dispSubcat,delSubcat,editSubcat}
--- Project/routes/subcatRoutes.js
+++ Project/routes/subcatRoutes.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var router = express.Router()
-var {insSubcat,dispSubcat,delSubcat} = require('../controllers/subcatController')
+var {insSubcat,dispSubcat,delSubcat,editSubcat} = require('../controllers/subcatController')
 router.get('/',dispSubcat)
 router.post('/ins',insSubcat)
 router.get('/del/:id',delSubcat)
+router.get('/edit/:id',editSubcat)
 
 module.exports = router
