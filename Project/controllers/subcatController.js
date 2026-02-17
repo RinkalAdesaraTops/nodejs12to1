de4a8311160dd3a222ce29a42267ade710ff4dbc
Rinkal <rinkaladesara.tops@gmail.com>
2026-02-17 12:29:46 +0530

    img crud

--- Project/controllers/subcatController.js
+++ Project/controllers/subcatController.js
@@ -1,38 +1,55 @@
 const subcatModel = require('../models/subcatModel')
+const fs = require('fs')
 // import subcatModel from "../models/subcatModel.js"
-const insSubcat=async(req,res)=>{
+const insSubcat = async (req, res) => {
     let id = req.body.subid
     console.log(req.file);
-    if(req.file != undefined){
+    if (req.file != undefined) {
         req.body.image = req.file.filename
+        if (id != '') {
+            let data = await subcatModel.findById(id)
+            if (data.image != '') {
+                fs.unlink("public/uploads/" + data.image, (err) => {
+                    if (err) console.log("error == " + err)
+                    console.log('image deleted');
+                })
+            }
+        }
     }
-    if(id!=''){
-        let ans = await subcatModel.findByIdAndUpdate(id,req.body)
+    if (id != '') {
+        let ans = await subcatModel.findByIdAndUpdate(id, req.body)
     } else {
         let ans = await subcatModel.insertOne(req.body)
     }
     res.redirect('/subcategory')
 }
-const dispSubcat=async(req,res)=>{
+const dispSubcat = async (req, res) => {
     let data = await subcatModel.find()
-    res.render('subcategory',{
-        "subcatdata":data,
-         "editdata":""
+    res.render('subcategory', {
+        "subcatdata": data,
+        "editdata": ""
     })
 }
-const delSubcat = async(req,res)=>{
+const delSubcat = async (req, res) => {
     let id = req.params.id
+    let subcatData = await subcatModel.findById(id)
+    if (subcatData.image != '') {
+        fs.unlink("public/uploads/" + subcatData.image, (err) => {
+            if (err) console.log("error == " + err)
+            console.log('image deleted');
+        })
+    }
     let data = await subcatModel.findByIdAndDelete(id)
     res.redirect('/subcategory')
 }
 
-const editSubcat = async(req,res)=>{
-   const id = req.params.id
-     let editdata = await subcatModel.findById(id)
+const editSubcat = async (req, res) => {
+    const id = req.params.id
+    let editdata = await subcatModel.findById(id)
     let data = await subcatModel.find()
-    res.render('subcategory',{
-        subcatdata:data,
-        editdata:editdata
+    res.render('subcategory', {
+        subcatdata: data,
+        editdata: editdata
     })
 }
-module.exports = {insSubcat,dispSubcat,delSubcat,editSubcat}
+module.exports = { insSubcat, dispSubcat, delSubcat, editSubcat }
